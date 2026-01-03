e2bbf88e5363dfa209dbe897df56bd3bb26e7192
Mohammed Nurul Islam <mohammed19.islam@gmail.com>
2026-01-03 20:09:41 +0000

    Fixed the buy ticker and reformat

--- stockyapi/Application/Portfolio/BuyTicker/BuyTickerRequest.cs
+++ stockyapi/Application/Portfolio/BuyTicker/BuyTickerRequest.cs
@@ -1,19 +1,19 @@
 using System.ComponentModel.DataAnnotations;
-using MediatR;
-using stockyapi.Responses;
 
 namespace stockyapi.Requests;
 
-public class BuyTickerRequest : IRequest<BuyTickerResponse>
+public class BuyTickerRequest
 {
+    private const double MaxValue = 100000;
+    
     [Required]
     public required string Symbol { get; set; }
 
     [Required]
-    [Range(0, double.MaxValue, ErrorMessage = "Quantity must be at least 1")]
+    [Range(0, MaxValue, ErrorMessage = "Quantity must be at least 1")]
     public required decimal Quantity { get; set; }
 
     [Required]
-    [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0")]
+    [Range(0.01, MaxValue, ErrorMessage = "Price must be greater than 0")]
     public required decimal Price { get; set; }
 }
