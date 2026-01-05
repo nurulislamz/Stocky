c1b64c9cb32e578882926cee4e7b87497b58f189
Mohammed Nurul Islam <mohammed19.islam@gmail.com>
2026-01-05 19:54:36 +0000

    Refactors

---
+++ stockyapi/Application/Auth/Register/RegisterResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace stockyapi.Responses;
+
+public class RegisterResponse(string token, string email, string userId)
+{
+   [Required]
+   public string Token { get; init; } = token;
+
+   [Required, EmailAddress]
+   public string Email { get; init; } = email;
+
+   [Required]
+   public string UserId { get; init; } = userId;
+}
--- stockyapi/DTOs/Requests/AccountSettingsRequest/SetOpenAiApiKeyRequest.cs
+++
@@ -1,9 +0,0 @@
-using MediatR;
-using stockyapi.Responses;
-
-namespace stockyapi.Requests;
-
-public class SetOpenAiApiKeyRequest : IRequest<SetOpenAiApiKeyResponse>
-{
-    public required string ApiKey { get; set; }
-}
