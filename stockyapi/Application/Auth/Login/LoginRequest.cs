5c22ccf847feb736265bdc2930c2b8c3c7a9cffe
Mohammed Nurul Islam <mohammed19.islam@gmail.com>
2026-01-14 19:56:08 +0000

    LoginEndpoint cleaned up and refactored

---
+++ stockyapi/Application/Auth/Login/LoginRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace stockyapi.Requests;
+
+public class LoginRequest
+{
+   [Required, EmailAddress]
+   public required string Email { get; init; }
+
+   [Required, MinLength(8)]
+   public required string Password { get; init; }
+}
---
+++ stockyapi/Application/Auth/Login/LoginResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace stockyapi.Responses;
+
+public class LoginResponse(string token)
+{
+    [Required]
+    private string Token { get; init; } = token;
+};
