f81362999286034c2464c54a549c9a46ed5c9d56
Mohammed Nurul Islam <mohammed19.islam@gmail.com>
2026-01-19 19:11:06 +0000

    yahoo endpoint, needs to be fixed

---
+++ stockyapi/Services/YahooFinance/IYahooFinanceRepository.cs
@@ -0,0 +1,9 @@
+﻿using stockyapi.Responses;
+
+namespace stockyapi.Repository.YahooFinance;
+
+public interface IYahooFinanceRepository
+{
+    Task<(CurrentPriceData? Data, string? Error)> GetCurrentTickerPriceAsync(string ticker);
+    Task<(HistoricalPriceData? Data, string? Error)> GetHistoricalTickerPriceAsync(string ticker, string startTimestamp, string endTimestamp);
+}
