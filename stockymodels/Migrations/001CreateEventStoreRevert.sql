cb9d378dd623906ea999f02b0b5ef80580dc05e4
Mohammed Nurul Islam <mohammed19.islam@gmail.com>
2026-03-20 01:16:05 +0000

    removed ef/ ef migrations and fixed the docker to use latest

--- stockymodels/Migrations/001CreateEventStoreRevert.sql
+++
@@ -1,4 +0,0 @@
-DROP POLICY IF EXISTS event_user_policy ON "Events";
-DROP TABLE IF EXISTS "Events";
-DROP TABLE IF EXISTS "Commands";
-DROP SCHEMA stockydb cascade;
--- stockymodels/Migrations/002CreateProjectionsRevert.sql
+++
@@ -1,8 +0,0 @@
-SET search_path TO stockydb;
-
-DROP TABLE IF EXISTS "PriceAlerts";
-DROP TABLE IF EXISTS "Watchlist";
-DROP TABLE IF EXISTS "UserPreferences";
-DROP TABLE IF EXISTS "StockHoldings";
-DROP TABLE IF EXISTS "Portfolios";
-DROP TABLE IF EXISTS "Users";
